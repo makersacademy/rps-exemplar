38d0d0afde0a02a5354a3b24b5e156a5fe8bd5b5
jafrog <howeveririna@gmail.com>
2015-11-04 13:47:34 +0000

    User Story 3
    As a marketeer
    So I can play a game
    I want to choose a shape to play

--- spec/feature/play_spec.rb
+++ spec/feature/play_spec.rb
@@ -9,12 +9,21 @@ feature 'playing a game' do
     fill_in 'name', with: 'Dave'
     click_button 'Submit'
   end
+
   # As a marketeer
   # So I can play a game
   # I want to see the shapes I can play
   scenario 'see the shape options' do
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissors'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
+  end
+
+  # As a marketeer
+  # So I can play a game
+  # I want to choose a shape to play
+  scenario 'choose a shape' do
+    click_button 'Rock'
+    expect(page).to have_content 'You chose Rock!'
   end
 end
