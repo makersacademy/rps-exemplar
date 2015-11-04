2ddceea980042a67cb8810cb8b77a5c5b4a3d4a3
jafrog <howeveririna@gmail.com>
2015-11-04 13:39:37 +0000

    User Story 2
    As a marketeer
    So I can play a game
    I want to see the shapes I can play

---
+++ spec/feature/play_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
+feature 'playing a game' do
+  before do
+    visit '/'
+    fill_in 'name', with: 'Dave'
+    click_button 'Submit'
+  end
+  # As a marketeer
+  # So I can play a game
+  # I want to see the shapes I can play
+  scenario 'see the shape options' do
+    expect(page).to have_content 'Rock'
+    expect(page).to have_content 'Paper'
+    expect(page).to have_content 'Scissors'
+  end
+end
