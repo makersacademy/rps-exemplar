ed4dbf5068ee9e9229a0aa18cdf84529f8034fcb
jafrog <howeveririna@gmail.com>
2015-11-04 14:04:52 +0000

    User Story 5
    As a marketeer
    So I can play a game
    I want the game to choose a random option

--- spec/feature/play_spec.rb
+++ spec/feature/play_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 # So that I can enjoy myself away from the daily grind
 # I would like to be able to play rock/paper/scissors
 feature 'playing a game' do
+  PLAY_SEED = 221563
+
   before do
     visit '/'
     fill_in 'name', with: 'Dave'
@@ -36,6 +38,15 @@ feature 'playing a game' do
     message = find(:css, "#opponent").text
 
     expect(possible_messages).to include message
+  end
+
+  # As a marketeer
+  # So I can play a game
+  # I want the game to choose a random option
+  scenario 'game chooses a random option' do
+    srand(PLAY_SEED)
+    click_button 'Rock'
+    expect(page).to have_content 'Opponent chose Scissors!'
   end
 
   def possible_messages
